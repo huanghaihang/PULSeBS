b56c2168100139227cc27b209eaafaf0ac2cfff0
giovannitangredi <62295204+giovannitangredi@users.noreply.github.com>
2020-11-10 13:51:05 +0100

    [PULSEBS-31] fix: small readability changes

--- src/components/ReservationPage.tsx
+++ src/components/ReservationPage.tsx
@@ -3,16 +3,35 @@ import {BookingLectureList} from "./BookingLectureList"
 import {BookedLectureList} from "./BookedLectureList"
 import Container from "react-bootstrap/Container"
 export const ReservationPage = (props:any)=>{
-   
+    const getReservations = ()=>{
+        setLoading(true);
+        setLectures(FacReservations);
+        setBookedLectures(FacReservations);
+        setLoading(false)
+      }
+      let FacReservations = [{course : "Softwere Engeneering 2", booked_students :20,capacity: 45, id:1 ,start:  "2020-11-11:08:30:00", end : "2020-11-11:11:00:00", lecturer : "Mario Rossi", name : "Lecture 1" },
+      {course : "Softwere Engeneering 2",booked_students :3, capacity: 45,id:2 ,start: "2020-11-14:08:30:00",end: "2020-11-14:11:00:00",lecturer:"Mario Rossi",name:"Lecture 2"},
+      {course : "Web Application 1",booked_students :43,capacity: 61 ,id:3 ,start: "2020-11-12:08:30:00",end: "2020-11-12:11:00:00",lecturer:"Valeria Verdi",name:"Lecture 1"}];
+        const [bookedLectures,setBookedLectures] = useState<any>([]);
+        const [lectures,setLectures] = useState<any>([]);
+        const [loading,setLoading]= useState(false);
+        
+        const bookLecture = (lectureID : any,studentID : any) =>{
+          //TO DO
+          console.log(lectureID);
+        }
     useEffect(()=>{
-        props.getReservations();
+        getReservations();
     },[]) 
+
+  if(loading)
+    return(<div>Page is loading...</div>)
     return (
         <Container>
             <h1>Book a Lecture</h1>
-            <BookingLectureList lectures={props.lectures} bookLecture= {props.bookLecture}></BookingLectureList>
+            <BookingLectureList lectures={lectures} bookLecture= {bookLecture}></BookingLectureList>
             <h1>Booked Lectures</h1>
-            <BookedLectureList  lectures={props.bookedLectures}></BookedLectureList>
+            <BookedLectureList  lectures={bookedLectures}></BookedLectureList>
         </Container>
     );
 }
--- src/components/app.tsx
+++ src/components/app.tsx
@@ -6,25 +6,8 @@ import "./../styles/styles.css";
 
 // Create App component
 export const App = () => {
-  let FacReservations = [{course : "Softwere Engeneering 2", booked_students :20,capacity: 45, id:1 ,start:  "2020-11-11:08:30:00", end : "2020-11-11:11:00:00", lecturer : "Mario Rossi", name : "Lecture 1" },
-{course : "Softwere Engeneering 2",booked_students :3, capacity: 45,id:2 ,start: "2020-11-14:08:30:00",end: "2020-11-14:11:00:00",lecturer:"Mario Rossi",name:"Lecture 2"},
-{course : "Web Application 1",booked_students :43,capacity: 61 ,id:3 ,start: "2020-11-12:08:30:00",end: "2020-11-12:11:00:00",lecturer:"Valeria Verdi",name:"Lecture 1"}];
-  const [bookedLectures,setBookedLectures] = useState<any>([]);
-  const [lectures,setLectures] = useState<any>([]);
-  const [loading,setLoading]= useState(false);
-  const getReservations = ()=>{
-    setLoading(true);
-    setLectures(FacReservations);
-    setBookedLectures(FacReservations);
-    setLoading(false)
-  }
-  const bookLecture = (lectureID : any,studentID : any) =>{
-    //TO DO
-    console.log(lectureID);
-  }
-  if(loading)
-    return(<div>Page is loading...</div>)
+  
   return <div className="app">
-    <ReservationPage bookedLectures = {bookedLectures} lectures={lectures} getReservations={getReservations} bookLecture = {bookLecture}></ReservationPage>
+    <ReservationPage></ReservationPage>
   </div>;
 };
