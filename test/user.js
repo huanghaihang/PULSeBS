f8a44897ba26ef78fa8f67c3f5fc0fc6ea209998
Enrico Carraro <enrico.carraro@studenti.polito.it>
2020-11-11 15:18:14 +0100

    Fix testing with authentication and add tests.

--- test/user.js
+++ test/user.js
@@ -4,29 +4,56 @@ const expect = chai.expect;
 chai.use(chaiHttp);
 const app = require("../server/server.js");
 const knex = require("../server/db");
-var request=require('supertest'); 
+var request = require("supertest");
 
 //the data we need to pass to the login method
 const userCredentials = {
-  email: 's274930@studenti.polito.it', 
-  password: 'password'
-}
+  email: "s274930@studenti.polito.it",
+  password: "password",
+};
+
+const userTuple = {
+  id: 1,
+  name: "Enrico",
+  surname: "Carraro",
+  password_hash: "$2b$10$A9KmnEEAF6fOvKqpUYbxk.1Ye6WLHUMFgN7XCSO/VF5z4sspJW1o.",
+  email: "s274930@studenti.polito.it",
+  role: "student",
+};
+
+const userResponseData = { ...userTuple };
+delete userResponseData.password_hash;
+
 //now let's login the user before we run any tests
-var authenticatedUser = request.agent(app);
-before(function(done){
-  authenticatedUser
-    .post('/api/auth/login')
-    .send(userCredentials)
-    .end(function(err, response){
-      expect(response.statusCode).to.equal(200);
-      //expect('Location', '/');
-      done();
-    });
+const authenticatedUser = request.agent(app);
+beforeEach(async () => {
+  await knex("user").del();
+  await knex("user").insert(userTuple);
+  const res = await authenticatedUser
+    .post("/api/auth/login")
+    .send(userCredentials);
+
+  expect(res.status).to.equal(200);
 });
 
-describe('GET /api/user', function(done){
-  it('should return a 200 response if the user is logged in', function(done){
-    authenticatedUser.get('/api/user')
-    .expect(200, done);
+describe("GET /api/user", async () => {
+  it("should return a 200 response with the user data if the user is logged in.", async () => {
+    const res = await authenticatedUser.get("/api/user");
+    expect(res.status).to.equal(200);
+    expect(res.body).to.eql(userResponseData);
+  });
+
+  it("should return a 401 response when the user is not authenticated.", async () => {
+    const res = await request(app).get("/api/user");
+    expect(res.status).to.equal(401);
+    expect(res.body).to.eql({ message: "Authorization error" });
+  });
+
+  it("should return a 401 response after the user logs out.", async () => {
+    const resLogout = await authenticatedUser.post("/api/auth/logout");
+    const resUserData = await authenticatedUser.get("/api/user");
+    expect(resLogout.status).to.equal(200);
+    expect(resUserData.status).to.equal(401);
+    expect(resUserData.body).to.eql({ message: "Authorization error" });
   });
 });
