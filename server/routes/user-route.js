134fa9becdc5fed21ebf00adad366d0a6ed29f6a
Haihang Huang <s273677@studenti.polito.it>
2020-11-16 20:49:25 +0800

    login router

--- server/routes/user-route.js
+++ server/routes/user-route.js
@@ -8,6 +8,27 @@ const userRoutes = require("./../controllers/user-controller");
 const router = express.Router();
 
 router.get("/", userRoutes.get);
-
+//login API
+router.post('/login', function(req, res, next){
+    var data = {
+        username: req.body.username,
+        password: req.body.password
+    }
+    module.connect(function(db){
+        db.collection('user').find(data).toArray(function(err, docs){
+            if(err){
+                res.redirect('/login')
+            }else{
+                if(docs.length>0){
+                    res.redirect('/')
+                }else{
+                    res.redirect('login')
+                }
+            }
+        })
+    })
+ 
+})
 // Export router
 module.exports = router;
+
--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,10 +1,14 @@
 // Import deps
 import React, { useEffect, useState } from "react";
-
+import { LoginPage } from "./LoginPage";
 // Import styles
 import "./../styles/styles.css";
 
 // Create App component
 export const App = () => {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <LoginPage></LoginPage>
+    </div>
+  );
 };
